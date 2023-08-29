1d70aaa5e722c3d51d476ca1d7adae3068bb2638
name <asdfad@asdfasd.com>
2023-08-29 01:32:42 -0700

    ok

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
 {-# LANGUAGE DuplicateRecordFields #-} 
+{-# LANGUAGE QuasiQuotes #-} -- support raw string [r|<p>dog</p> |]
 
 -- import Data.Set   -- collide with Data.List 
 import Control.Monad
@@ -48,6 +49,7 @@ import Network.HTTP.Types.Status (statusCode)
 import Network.HTTP.Types.Header
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Client
+import Text.RawString.QQ (r)         -- Need QuasiQuotes too 
 
 
 import AronModule 
@@ -60,9 +62,9 @@ firstCodeBlock :: [String] -> [String]
 firstCodeBlock cx = isH ? lt $ error "Invalid Snippet Format"
     where
       lr    = trimBothEnd cx
-      isH   = len lr > 1 ? (isHeaderFstLine $ head lr) $ error "Invalid Snippet Format"
-      -- ls    = filter (\x -> len x > 0) $ map trimList $ splitWhen isHeaderFstLine cx
-      ls    = filter (\x -> len x > 0) $ map trimBothEnd $ splitWhen isHeaderFstLine cx
+      isH   = len lr > 1 ? (isValidHeader $ head lr) $ error "Invalid Snippet Format"
+      -- ls    = filter (\x -> len x > 0) $ map trimList $ splitWhen isValidHeader cx
+      ls    = filter (\x -> len x > 0) $ map trimBothEnd $ splitWhen isValidHeader cx
       lt    = (take 1 lr) ++ (head ls)
 
 take2 :: String -> [String]
@@ -77,9 +79,9 @@ take2 (a:b:cx) = [a,b] : take2 (b:cx)
         ab:*:cd  @= 123
     @
 -}
-isHeaderFstLine :: String -> Bool
-isHeaderFstLine [] = False
-isHeaderFstLine cx = c2 == 2 && d1 == 1 
+isValidHeader :: String -> Bool
+isValidHeader [] = False
+isValidHeader cx = (c2 == 2 && d1 == 1) || c2 == 2
     where 
       c2 = sum $ map (\x -> x == ':' ? 1 $ 0) cx
       d1 = sum $ map (\x -> x == "@=" ? 1 $ 0) $ take2 cx
@@ -88,6 +90,20 @@ isHeaderFstLine cx = c2 == 2 && d1 == 1
                                                                            Nothing -> error "ERROR: splitWhenFirstNoRegex"
       num = snd lt 
       isNum = foldr (\a b -> a && b) True $ map isDigit num
+
+
+extractHead :: String -> (Bool, String, Integer)
+extractHead [] = (False, [], 0)
+extractHead cx = tu 
+    where 
+      b1 = let n = sum $ map (\x -> x == ':' ? 1 $ 0) cx in n == 2
+      b2 = let n = sum $ map (\x -> x == "@=" ? 1 $ 0) $ take2 cx in n == 1
+
+      tu = b1 && b2 ? (let s = join (***) trim $ let ma = splitWhenFirstNoRegex "@=" cx in case ma of
+                                                                                          Just x -> x
+                                                                                          Nothing -> error "ERROR: extractHeadX"
+                                             
+                       in let sn = snd s in (True, fst s, read sn :: Integer)) $  (b1 ?  (True, cx, 0) $ (False, [], 0))
 
 {-|
     ===
@@ -100,21 +116,28 @@ isHeaderFstLine cx = c2 == 2 && d1 == 1
 -}
 extractHeadX :: String -> (Bool, String, Integer)
 extractHeadX [] = (False, [], 0)
-extractHeadX st = isHeaderFstLine st ? ( let s = join (***) trim $ let ma = splitWhenFirstNoRegex "@=" st in case ma of
+extractHeadX st = isValidHeader st ? ( let s = join (***) trim $ let ma = splitWhenFirstNoRegex "@=" st in case ma of
                                                                                                               Just x -> x
                                                                                                               Nothing -> error "ERROR: extractHeadX"
                                                                  
                                          in let sn = snd s in (True, fst s, read sn :: Integer)) $ (False, [], 0)
+                                         --                   a:*:b@=3
+                                         --                   (True, a:*:b, 3)
 
 isHeaderT :: TS.Text -> Bool
-isHeaderT cs = isHeaderFstLine cx' 
+isHeaderT cs = isValidHeader cx' 
    where 
       cx' = strictTextToStr cs
 
+{-|
+ - If the first line has no @= then, it is 'insertcode'
+ -                                   else  'updatecode' 
+ -}
 postCodeBlockX :: String -> [String] -> IO()
 postCodeBlockX url lt = do
                 if len lt > 1 then do
-                    let tu@(isH, fstLine, pid) = extractHeadX $ head lt 
+                    -- let tu@(isH, fstLine, pid) = extractHeadX $ head lt 
+                    let tu@(isH, fstLine, pid) = extractHead $ head lt 
                     fw "tu"
                     pre tu 
                     if isH then do
@@ -123,11 +146,13 @@ postCodeBlockX url lt = do
                         let code = unlines $ fstLine:tt 
                         fw "code"
                         pre code 
+                        let cmd = pid == 0 ? "insertcode" $ "updatecode"
+                        let furl = url </> cmd
                         let block = UpdateCodeBlock{pid = pid, newcode = code, begt = 0, endt = 0} 
                         fw "block"
                         pre block
                         -- initReq <- parseRequest "http://localhost:8081/updatecode"
-                        initReq <- parseRequest url 
+                        initReq <- parseRequest furl 
                         let req = initReq { method = "POST", requestBody = RequestBodyLBS $ encode block}
                         response <- httpLbs req manager
                         putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
@@ -188,10 +213,11 @@ trimBothEnd = g . f
     f ls = dropWhile (\x -> (len . trim) x == 0) ls 
     g = reverse . f . reverse
 
+
 main = do
         argList <- getArgs
         case len argList of
-            v | v == 2 -> do
+            v | v == 30 -> do
                 let url = head argList 
                 let tmpfile = last argList
                 ls <- readFileSTextList tmpfile >>= \x -> return $ map strictTextToStr x
@@ -209,6 +235,26 @@ main = do
                 postCodeBlockX url lt' 
                 -- END_out1
               | v == 1 -> do
+                let a1 = head argList
+                let a2 = last argList
+                let urlx = a1 == "-d" ? "http://localhost:8080" $ (a1 == "-t" ? "http://localhost:8081" $ "")
+                ls <- getContents >>= return . lines
+                -- let lt = trimList ls
+                let lt = trimBothEnd ls
+                fw "lt"
+                pre lt
+                postCodeBlockX urlx lt
+
+              | v == 2 && head argList == "-u" -> do
+                let url = (head . tail) argList
+                let tmpfile = last argList
+                -- ls <- readFileSTextList tmpfile >>= \x -> return $ map strictTextToStr x
+                ls <- getContents >>= return . lines
+                let lt = trimBothEnd ls
+                let lt' = firstCodeBlock lt
+                postCodeBlockX url lt' 
+
+              | v == 10 -> do
                 let url = head argList 
                 -- pipe as stdInput
                 ls <- getContents >>= return . lines
@@ -220,17 +266,64 @@ main = do
                 pre lt
                 postCodeBlockX url lt
               | otherwise -> do
+                putColor 150 "\tpostSnippet -d  /tmp/a  => -d [default] => http://localhost:8080"
+                putColor 150 "\tpostSnippet -t  /tmp/a  => -t [test]    => http://localhost:8081"
+                putColor 170 "\techo /tmp/a | postSnippet -d"
+                putColor 170 "\techo /tmp/a | postSnippet -t"
+                putColor 100 ""
+                putColor 100 "\t("
+                putColor 100 "\t)"
                 putColor 140 "\tTwo arguments :"
-                putColor 200 "\tpostSnippet http://localhost:8081/updatecode  /tmp/x1.x                                 "
+                putColor 200 "\tpostSnippet http://localhost:8081               /tmp/x1.x                                 "
                 putColor 100 "\t                     ↑                            ↑                                     "
                 putColor 100 "\t                     + -> Update code URL          + -> tmp file store edited snippet    "
                 putColor 100 "\t"
                 putColor 100 "\t"
                 putColor 140 "\tOne arguments => Stdin from Pipe"
-                putColor 200 "\tcat /tmp/x1.x | postSnippet http://localhost:8081/updatecode                             "
+                putColor 200 "\tcat /tmp/x1.x | postSnippet http://localhost:8081                             "
                 putColor 100 "\t"
-                putColor 130 "\tThere are at least two lines in an input file"
+                putColor 130 "\tInput file must contains at least two lines"
                 putColor 100 "\t"
                 putColor 140 "\tSee => HttpSnippetX() and UpdateSnippet()"
                 putColor 100 "\tVerson: 1.0"
                 putColor 100 "\t"
+                let s = [r| 
+                
+                            postSnippet -d  /tmp/a  => -d [default] => http://localhost:8080
+                            postSnippet -t  /tmp/a  => -t [test]    => http://localhost:8081
+                            echo /tmp/a | postSnippet -d
+                            echo /tmp/a | postSnippet -t
+
+                            Bash
+                            (
+                            echo 'a:*: test'
+                            echo 'line 1'
+                            ) | postSnippet -d
+                            
+                            Two arguments :
+                            postSnippet http://localhost:8081               /tmp/x1.x                                 
+                                                 ↑                            ↑                                    
+                                                 + -> Update code URL          + -> tmp file store edited snippet    
+                            
+                            One arguments => Stdin from Pipe
+                            cat /tmp/x1.x | postSnippet http://localhost:8081                             
+                            
+                            Input file must contains at least two lines
+                            
+                            See => HttpSnippetX() and UpdateSnippet()
+                        |] 
+                let ss = lines s
+                mapM_ putStrLn ss 
+{--
+main = do
+       pp "ok"
+       when True $ do
+           let s = "a::b"
+           print $ extractHead s
+       when True $ do
+           let s1 = "a::b@=3"
+           print $ extractHead s1
+       when True $ do
+           let s1 = "a:b@=3"
+           print $ extractHead s1
+--}
